d2daeca5499f359448116a49fdb9ad883c8b8e63
darcyjwood <darcyjwood@gmail.com>
2022-09-12 20:31:26 +0000

    changed capacity to 5

--- compute/main.tf
+++ compute/main.tf
@@ -59,7 +59,7 @@ resource "aws_autoscaling_group" "demo_database" {
   vpc_zone_identifier = tolist(var.public_subnet)
   min_size            = 2
   max_size            = 3
-  desired_capacity    = 2
+  desired_capacity    = 5
 
   launch_template {
     id      = aws_launch_template.demo_database.id
