dd6c26bb0b4d8214be1a043c9ce612777ff2f258
darcyjwood <darcyjwood@gmail.com>
2022-09-20 19:26:04 +0000

    updates

--- backends.tf
+++ backends.tf
@@ -1,12 +1,4 @@
 
-# terraform{
-#     required_version = ">=0.12.0"
-#   backend "s3" {
-#     bucket = "djw-terraform-project"
-#     key    = "backend-tf-state"
-#     region = "us-east-1"
-#   }
-# }
 
 data "terraform_remote_state" "network" {
   backend = "s3"
